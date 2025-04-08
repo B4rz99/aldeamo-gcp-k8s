af2a6fc686d0bef3fff3042a45a09cda25ad06df
obarbozaa <obarbozaa@gmail.com>
2025-04-07 19:14:01 -0500

    Fixed terraform version and commented not needed outputs and variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "proxy_ip" {
-  value = module.compute.public_ip
-}
+# output "proxy_ip" {
+#   value = module.compute.public_ip
+# }
 
-output "ingress_ip" {
-  value = module.gke.ingress_ip
-}
+# output "ingress_ip" {
+#   value = module.gke.ingress_ip
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,22 +8,22 @@ variable "region" {
   type        = string
 }
 
-variable "zone" {
-  description = "The GCP zone to deploy zonal resources in"
-  type        = string
-}
-
-variable "ssh_user" {
-  description = "The SSH username for connecting to the Compute Engine instance"
-  type        = string
-}
-
-variable "ssh_public_key_path" {
-  description = "Path to the public SSH key for the proxy VM"
-  type        = string
-}
-
-variable "domain" {
-  description = "The domain name (e.g. www.example.com) for the HTTPS proxy"
-  type        = string
-}
+ variable "zone" {
+   description = "The GCP zone to deploy zonal resources in"
+   type        = string
+ }
+# 
+# variable "ssh_user" {
+#   description = "The SSH username for connecting to the Compute Engine instance"
+#   type        = string
+# }
+# 
+# variable "ssh_public_key_path" {
+#   description = "Path to the public SSH key for the proxy VM"
+#   type        = string
+# }
+# 
+# variable "domain" {
+#   description = "The domain name (e.g. www.example.com) for the HTTPS proxy"
+#   type        = string
+# }
