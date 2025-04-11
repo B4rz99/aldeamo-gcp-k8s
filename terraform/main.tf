6f6239c14dcd8462884c274d4e8a71dbdf15e4aa
obarbozaa <obarbozaa@gmail.com>
2025-04-11 10:08:08 -0500

    Setup terraform backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "aldeamo-terraform"
+    prefix = "terraform/state"
+  }
+}
+
 # Configures the GCP provider with the specified project and region.
 provider "google" {
   project = var.project_id
@@ -18,7 +25,6 @@ module "compute" {
   subnet = module.network.subnet_id
   ssh_user = var.ssh_user
   ssh_public_key_path = var.ssh_public_key_path
-
   # Ensures network resources are created before the compute instance is provisioned
   depends_on = [module.network]
 }
